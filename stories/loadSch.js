dd88aca5b7ccba68431743c9fe3743b63fb22811
AntonC9018 <anton.curm@gmail.com>
2018-11-03 18:43:33 +0200

    Load schedule from json

---
+++ stories/loadSch.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+  $.getJSON("./stories/sch.json", function(data) {
+    let d = data.data;
+
+    for (let i = 0; i < d.length; i++) {
+      let box = $('<div>')
+        .addClass('box')
+        .append($('<div>')
+          .addClass('day-title')
+          .html($('<strong>')
+            .html(d[i][0])));
+
+      let obj = $("<div>").addClass("objects");
+
+      for (let j = 1; j < d[i].length; j++) {
+        obj.append($("<div>")
+          .addClass(d[i][j][0]) // set class to short name
+          .html(d[i][j][1])) // set the full name
+      }
+      obj.appendTo(box);
+      $(".schedule-page").append(box);
+
+      $(".box").css("margin", "15px");
+      $(".day-title").css({
+        "text-align": "center",
+        "margin-bottom": "10px"
+      });
+    }
+  })
+})
